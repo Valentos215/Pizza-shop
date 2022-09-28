c9f0b637fff29e6b56f7bd096e4169468954a060
valentin_215 <teskavale46@gmail.com>
2022-09-28 20:40:48 +0300

    Validation and mask

--- src/components/Checkout/Form/Form.tsx
+++ src/components/Checkout/Form/Form.tsx
@@ -6,6 +6,9 @@ import Adress from "./Adress";
 import { CartContext } from "../../../contexts/cartContext";
 import { totalAmount } from "../../../utils";
 import Store from "./Store";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import InputMask from "react-input-mask";
 
 const Form = () => {
   const [delivery, setDelivery] = useState(true);
@@ -17,7 +20,50 @@ const Form = () => {
     store: string;
   } | null>(null);
 
-  const disabled = (delivery && !deliveryAdress) || (!delivery && !storeAdress);
+  const errorMes = {
+    nameLength: "Name length 2-25 letters",
+    nameRequired: "Name is required",
+    emailLength: "25 characters or less",
+    emailRequired: "Email is required",
+    emailIncorrect: "Incorrect email",
+    phoneRequired: "Phone number is required",
+  };
+
+  const adressError =
+    (delivery && !deliveryAdress) || (!delivery && !storeAdress);
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      phone: "",
+      email: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .min(2, errorMes.nameLength)
+        .max(25, errorMes.nameLength)
+        .required(errorMes.nameRequired),
+      email: Yup.string()
+        .max(25, errorMes.emailLength)
+        .email(errorMes.emailIncorrect)
+        .required(errorMes.emailRequired),
+      phone: Yup.string().required(errorMes.phoneRequired),
+    }),
+    onSubmit: (values) => {
+      setCheck(true);
+      if (!adressError) console.log(values);
+    },
+  });
+
+  const disabled = adressError || !formik.isValid;
+
+  let nameError =
+    (formik.touched.name && formik.errors.name) ||
+    formik.errors.name === errorMes.nameLength;
+  let emailError =
+    (formik.touched.email && formik.errors.email) ||
+    formik.errors.email === errorMes.emailLength;
+  let phoneError = formik.touched.phone && formik.errors.phone;
 
   return (
     <div className={s.wrapper}>
@@ -44,9 +90,51 @@ const Form = () => {
       </div>
       <h3>Contacts</h3>
       <form className={s.contacts}>
-        <input name="firstName" placeholder="First name"></input>
-        <input name="phone" placeholder="Phone"></input>
-        <input name="email" placeholder="E-mail"></input>
+        <div className={s.contacts__input}>
+          <span>{nameError && formik.errors.name}</span>
+          <input
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            name="name"
+            placeholder="First name"
+            className={nameError ? s.error : ""}
+            autoComplete="off"
+          ></input>
+        </div>
+        <div className={s.contacts__input}>
+          <span>{phoneError && formik.errors.phone}</span>
+          <InputMask
+            name="phone"
+            mask="+38(099) 999 99 99"
+            value={formik.values.phone}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            placeholder="Phone"
+            className={phoneError ? s.error : ""}
+          ></InputMask>
+          {/* <input
+            value={formik.values.phone}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            name="phone"
+            placeholder="Phone"
+            className={phoneError ? s.error : ""}
+            autoComplete="off"
+          ></input> */}
+        </div>
+        <div className={s.contacts__input}>
+          <span>{emailError && formik.errors.email}</span>
+          <input
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            name="email"
+            placeholder="E-mail"
+            className={emailError ? s.error : ""}
+            autoComplete="off"
+          ></input>
+        </div>
       </form>
       {delivery && <Adress />}
       {!delivery && (
@@ -64,7 +152,7 @@ const Form = () => {
         </p>
         <div
           className={disabled ? `${s.button} ${s.disabled}` : s.button}
-          onClick={() => setCheck(true)}
+          onClick={() => formik.handleSubmit()}
         >
           Checkout
         </div>
